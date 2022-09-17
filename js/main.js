3c614303c4203f1242287ada778a681e0d63a629
Pervaiz Farah <farahp1@outlook.com>
2022-09-17 02:28:29 -0400

    server changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-document.querySelector("#button").addEventListener("click", getMuscles)
+document.querySelector("h1").addEventListener("click", getMuscles)
 
 
 async function getMuscles() {
     const muscle = document.querySelector("input").value
     console.log(muscle)
-    const get = await fetch(`localhost:8000/api/${muscle}`)
+    const get = await fetch(`https://api-ultimate-workout.herokuapp.com/api/${muscle}`)
+    console.log("Fetch successful")
     const data = await get.json()
+    console.log(data)
     data.exercises.forEach(element => document.querySelector("#exercises").innerHTML += `<li>${element}<li>`)
 }
